8660e27c43baed77403ab3839f59e33e8bd613ba
Noah Watts <nwatts2@uccs.edu>
2023-02-14 13:58:55 -0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Nuah on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
